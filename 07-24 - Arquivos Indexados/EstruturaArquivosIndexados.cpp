7d739d8fca3b71660c247aeab85c5ae489265cec
diego <136401880+DKB-17@users.noreply.github.com>
2024-08-06 19:41:36 -0300

    Update EstruturaArquivosIndexados.cpp

--- 07-24 - Arquivos Indexados/EstruturaArquivosIndexados.cpp
+++ 07-24 - Arquivos Indexados/EstruturaArquivosIndexados.cpp
@@ -13,9 +13,9 @@ struct indice {
 };
 struct cliente {
     int codigo;
-    char nome[30];
-    char endereco[35];
-    char cidade[30];
+    string nome;
+    string endereco;
+    string cidade;
     char uf[2];
 
     void imprimir() const {
@@ -25,7 +25,7 @@ struct cliente {
 
 void buscaSerial(struct indice idx[], struct cliente cli[], int cont, int cod) {
     int i = 0;
-    for(;i < cont && cod > idx[i].codigo; i++)
+    for(;i < cont && cod > idx[i].codigo; i++);
     if(cod == idx[i].codigo) {
         cout << "\n\nCliente Encontrado";
         i = idx[i].ender;
@@ -39,19 +39,47 @@ void buscaSerial(struct indice idx[], struct cliente cli[], int cont, int cod) {
     }
     getch();
 }
+void buscaBinaria(struct indice idx[], struct cliente cli[], int contador, int cod) {
+
+    int i = 0;
+    int f = contador-1;
+    int meio = (i+f)/2;
+    for(; idx[meio].codigo != cod && i <= f; meio = (f+i)/2) {
+        if(idx[meio].codigo > cod) {
+            f = meio - 1;
+        }else {
+            i = meio + 1;
+        }
+    }
+    if(idx[meio].codigo == cod) {
+        cout << "\n\nCliente Encontrado";
+        i = idx[meio].ender;
+        cout << "\nCodigo do Cliente:" << cli[i].codigo;
+        cout << "\nNome:" << cli[i].nome;
+        cout << "\nEndereco:" << cli[i].endereco;
+        cout << "\nCidade:" << cli[i].cidade;
+        cout << "\nUf:" << cli[i].uf;
+    }else {
+        cout << "\n\n Cliente nao encontrado";
+    }
+    getch();
+}
 
 void leitura_dados(struct cliente cliente[],int contador) {
-    cout << "Leitura do cliente:";
-    cout << "Codigo:";
+    cout << "Leitura do cliente: ";
+    cout << "\nCodigo:";
     cin >> cliente[contador].codigo;
     cout << "Nome:";
-    gets(cliente[contador].nome);
+    fflush(stdin);
+    getline(cin,cliente[contador].nome);
     cout << "Endereco:";
-    gets(cliente[contador].endereco);
+    getline(cin,cliente[contador].endereco);
     cout << "Cidade:";
-    gets(cliente[contador].cidade);
-    cout << "Codigo:";
+    getline(cin,cliente[contador].cidade);
+    fflush(stdin);
+    cout << "Uf:";
     gets(cliente[contador].uf);
+
 }
 void leitura_indice(struct indice ind[], int contador) {
     cout << "Leitura ind:";
@@ -61,31 +89,39 @@ void leitura_indice(struct indice ind[], int contador) {
     cin >> ind[contador].ender;
 }
 
-void add_indice(struct indice ind[], int contador, struct cliente lsd[]){
-    int aux = 0;
-    for(;ind[contador-aux].codigo > lsd[contador].codigo && aux < contador ;aux++) {
-        ind[contador - (aux-1)] = ind[contador-aux];
+void ordenar_lista(struct indice ind[], int tamanho) {
+    indice aux;
+    for(int i = 0; i < tamanho;i++) {
+        for(int j = i + 1; j < tamanho; j++) {
+            if(ind[i].codigo > ind[j].codigo) {
+                aux = ind[i];
+                ind[i] = ind[j];
+                ind[j] = aux;
+            }
+        }
     }
-    ind[contador-aux].codigo = lsd[contador].codigo;
-    ind[contador-aux].ender = contador;
+}
+
+void add_indice(struct indice ind[], int contador, struct cliente lsd[]){
+    ind[contador].codigo = lsd[contador].codigo;
+    ind[contador].ender = contador;
 }
 
 int main() {
 
     struct indice ind[20];
     struct cliente dados[20];
-    int contador;
-    int ler;
+    int contador = 0;
+    int sair = 1;
 
-    cout << "Quantas leituras seram realizadas: ";
-    cin >> ler;
-
-    for(int i=0;i<ler;i++) {
-        system("cls");
-        cout << "\n\nLeitura da Area de dados";
-        leitura_dados(dados, contador);
-        add_indice(ind,contador,dados);
-        contador ++;
+    while(sair != 0) {
+        for(int i=0;i<ler;i++) {
+            system("cls");
+            leitura_dados(dados, contador);
+            add_indice(ind,contador,dados);
+            contador ++;
+            ordenar_lista(ind, contador);
+        }
     }
     for(int i=0; i < contador; i++) {
         cout << "\n";
@@ -93,6 +129,16 @@ int main() {
         cout << "\t";
         dados[i].imprimir();
     }
+
+    int codigobuscado;
+    cout << "\nDigite o codigo buscado: ";
+    cin >> codigobuscado;
+    buscaSerial(ind,dados,contador, codigobuscado);
+
+
+    cout << "\nDigite o codigo buscado: ";
+    cin >> codigobuscado;
+    buscaBinaria(ind,dados,contador, codigobuscado);
 
 }
 
